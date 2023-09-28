feb21712b16dc5ad1f95d57deea901a9ca82abc6
Ahmad Mash <wolfmashagbah@hotmail.com>
2023-09-28 10:47:09 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Write your solution in this file!
+const employee = {
+    name: 'Walter White',
+    streetAddress: 'ALBQ',
+}
+
+beforeEach(function() {
+    for (const key in employee) {
+        delete employee[key]
+    }
+
+    employee.name = 'Sam'
+    }
+)
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    
+    const nondestructive = {...employee};
+    nondestructive[key] = value
+    return nondestructive
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value
+    return employee
+}
+destructivelyUpdateEmployeeWithKeyAndValue(employee, 'streetAddress', '12 Broadway');
+
+function deleteFromEmployeeByKey(employee, key) {
+    let newEmployee = {...employee}
+    delete newEmployee[key]
+    return newEmployee
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    let newEmployee = employee
+    delete newEmployee[key]
+    return newEmployee
+}
+
